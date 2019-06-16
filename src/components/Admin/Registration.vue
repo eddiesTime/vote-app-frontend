9c0040eca5f419ac2769db517a98a78962fd7942
eddi <test@testing.com>
2019-06-16 19:40:42 +0200

    added vuetify

---
+++ src/components/Admin/Registration.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h3>Registration Vue</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Login from "./components/Login.vue";
+import Vote from "./components/Vote.vue";
+import Admin from "./components/Admin/Admin.vue";
+import Candidate from "./components/Admin/Candidate.vue";
+import District from "./components/Admin/District.vue";
+import Registration from "./components/Admin/Registration.vue";
+import Election from "./components/Admin/Election.vue";
 
 Vue.use(Router);
 
@@ -9,18 +15,51 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
-      path: "/",
-      name: "home",
-      component: Home
+      path: "*",
+      name: "login",
+      component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/",
+      redirect: "/vote",
+      name: "vote",
+      component: Vote
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/admin",
+      name: "admin",
+      component: Admin,
+      children: [
+        {
+          path: "",
+          redirect: "/election"
+        },
+        {
+          path: "/candidate",
+          name: "candidate",
+          component: Candidate
+        },
+        {
+          path: "/district",
+          name: "district",
+          component: District
+        },
+        {
+          path: "/election",
+          name: "election",
+          component: Election
+        },
+        {
+          path: "/registration",
+          name: "election",
+          component: Registration
+        }
+      ]
     }
   ]
 });
